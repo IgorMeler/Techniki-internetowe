e6ad224547832df8a78c5ae2dab771ae28e83180
Szymon Cogiel <szymi130801@gmail.com>
2023-01-11 19:42:15 +0100

    sidebar

--- frontend/src/common/b.js
+++ frontend/src/common/b.js
@@ -13,32 +13,32 @@ document.write(`
      <span class="tooltip">Search</span>
   </li>
   <li>
-  <a href="../index.html">
+  <a href="./index.html">
     <i class='bx bx-folder' ></i>
     <span class="links_name">Home</span>
   </a>
     <span class="tooltip">Home</span>
   </li>
   <li>
-    <a href="../dashboard/dashboard.html">
+    <a href="./dashboard/dashboard.html">
       <i class='bx bx-grid-alt'></i>
       <span class="links_name">Dashboard</span>
     </a>
      <span class="tooltip">Dashboard</span>
   </li>
   <li>
-   <a href="../User/User.html">
+   <a href="./User/User.html">
      <i class='bx bx-user' ></i>
      <span class="links_name">User</span>
    </a>
    <span class="tooltip">User</span>
  </li>
  <li>
-   <a href="../Tutorial/Tutorial.html">
+   <a href="./Tutorial/Tutorial.html">
      <i class='bx bx-chat' ></i>
-     <span class="links_name">Tutorial</span>
+     <span class="links_name">Messages</span>
    </a>
-   <span class="tooltip">Tutorial</span>
+   <span class="tooltip">Messages</span>
  </li>
  <li class="profile">
      <div class="profile-details">
